f7c884c6ce27f0ab0ede64758ae7a31d8b9e213c
Maciel <maciel_castro_junior@hotmail.com>
2018-11-01 16:28:51 -0300

    first commit in project  using eclipse-ide

---
+++ src/main/java/br/edu/unifeg/blogdaunifeg/config/ServeletSpringMVC.java
@@ -0,0 +1,23 @@
+package br.edu.unifeg.blogdaunifeg.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServeletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {AppWebConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+	
+
+}
