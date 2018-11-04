7b44153e7cd9f27ea1e763ec028c45a6061b929c
Maciel <maciel_castro_junior@hotmail.com>
2018-11-04 20:16:40 -0200

    Fixing hibernate bugs

--- src/main/java/br/edu/unifeg/blogdaunifeg/controllers/models/Users.java
+++ src/main/java/br/edu/unifeg/blogdaunifeg/controllers/models/Users.java
@@ -1,10 +1,15 @@
 package br.edu.unifeg.blogdaunifeg.controllers.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Users {
 
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
 	private int ra;
 	private String senha;
 	
